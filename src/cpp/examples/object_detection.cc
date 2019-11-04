20a9f10f1ee85a6c7067e889a5eb09725e2d4c4d
Nam Vu <vunam@google.com>
2019-11-04 12:04:41 -0600

    added dummy benchmark for object detection

--- src/cpp/examples/object_detection.cc
+++ src/cpp/examples/object_detection.cc
@@ -5,6 +5,7 @@
 #include "src/cpp/examples/label_utils.h"
 #include "src/cpp/examples/model_utils.h"
 #include "src/cpp/test_utils.h"
+#include <chrono>  // NOLINT
 
 ABSL_FLAG(std::string, model_path,
           "./test_data/ssd_mobilenet_v1_fine_tuned_edgetpu.tflite",
@@ -17,7 +18,7 @@ ABSL_FLAG(std::string, labels_path, "./test_data/pet_labels.txt",
           "Path to the imagenet labels.");
 
 void ObjectDetection(const std::string& model_path, const std::string& image_path,
-                   const std::string& labels_path) {
+                   const std::string& labels_path, const int num_inferences) {
   // Load the model.
   coral::DetectionEngine engine(model_path);
   std::vector<int> input_tensor_shape = engine.get_input_tensor_shape();
@@ -28,18 +29,24 @@ void ObjectDetection(const std::string& model_path, const std::string& image_pat
   // Read the label file.
   auto labels = coral::ReadLabelFile(labels_path);
 
-  auto results = engine.DetectWithInputTensor(input_tensor);
-  for (const auto& result : results) {
-    std::cout << "---------------------------" << std::endl;
-    std::cout << "Candidate: " << labels[result.label] << std::endl;
-    std::cout << "Score: " << result.score << std::endl;
-    std::cout << "Corner: " << result.corners.DebugString() << std::endl;
+  // timing 
+  const auto& start_time = std::chrono::steady_clock::now();
+  for (int i =0; i <= num_inferences; i++) {
+    engine.DetectWithInputTensor(input_tensor);
   }
+  std::chrono::duration<double> time_span =
+      std::chrono::steady_clock::now() - start_time;
+  std::cout << "Running " << model_path
+	    << " with " << num_inferences << " inferences"
+	    << " costs: " << time_span.count() << " seconds." << std::endl;
 }
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
-  ObjectDetection(absl::GetFlag(FLAGS_model_path),
-                absl::GetFlag(FLAGS_image_path),
-                absl::GetFlag(FLAGS_labels_path));
+  for (auto i : {100, 200, 400, 800, 1600, 3200}) {
+    ObjectDetection(absl::GetFlag(FLAGS_model_path),
+                  absl::GetFlag(FLAGS_image_path),
+                  absl::GetFlag(FLAGS_labels_path),
+		  i);
+  }
 }

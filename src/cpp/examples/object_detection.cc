0cfc07d977754bc9d2c346f4466bb2245ce2a294
Nam Vu <vunam@google.com>
2019-11-14 10:59:53 -0600

    quick script to text multiple inputs

--- src/cpp/examples/object_detection.cc
+++ src/cpp/examples/object_detection.cc
@@ -6,40 +6,60 @@
 #include "src/cpp/examples/model_utils.h"
 #include "src/cpp/test_utils.h"
 
+#include <dirent.h> // a little ugly but will do for now
+#include <stdio.h>
+#include <vector>
+
 ABSL_FLAG(std::string, model_path,
           "./test_data/ssd_mobilenet_v1_fine_tuned_edgetpu.tflite",
           "Path to the tflite model.");
 
-ABSL_FLAG(std::string, image_path, "./test_data/pets.bmp",
+ABSL_FLAG(std::string, images_path, "./test_data/bmp/",
           "Path to the image to be classified.");
 
-ABSL_FLAG(std::string, labels_path, "./test_data/pet_labels.txt",
-          "Path to the imagenet labels.");
+// this is a little ugly
+const std::vector<std::string> get_images(const std::string& images_path) {
+  struct dirent *entry = nullptr;
+  DIR *dp = nullptr;
 
-void ObjectDetection(const std::string& model_path, const std::string& image_path,
-                   const std::string& labels_path) {
+  std::vector<std::string> ret;
+
+  dp = opendir(images_path.c_str());
+  if (dp != nullptr) {
+    while ((entry = readdir(dp))) {
+      if (entry->d_name[0] != '.') // so hacky :(
+        ret.push_back({images_path + entry->d_name});
+    }
+  }
+
+  return ret;
+}
+
+void ObjectDetection(const std::string& model_path, const std::string& images_path) {
   // Load the model.
   coral::DetectionEngine engine(model_path);
   std::vector<int> input_tensor_shape = engine.get_input_tensor_shape();
-  // Read the image.
-  std::vector<uint8_t> input_tensor = coral::GetInputFromImage(
-      image_path,
-      {input_tensor_shape[1], input_tensor_shape[2], input_tensor_shape[3]});
-  // Read the label file.
-  auto labels = coral::ReadLabelFile(labels_path);
 
-  auto results = engine.DetectWithInputTensor(input_tensor);
-  for (const auto& result : results) {
+  // Get all images
+  const auto& images = get_images(images_path);
+  for (auto& image_path : images) {
+    std::cout << "Image: " << image_path << "\n";
+    // read current images
+    std::vector<uint8_t> input_tensor = coral::GetInputFromImage(
+        image_path,
+        {input_tensor_shape[1], input_tensor_shape[2], input_tensor_shape[3]});
+
+    auto results = engine.DetectWithInputTensor(input_tensor);
+    for (const auto& result : results) {
+      std::cout << "Score: " << result.score << std::endl;
+      std::cout << "Corner: " << result.corners.DebugString() << std::endl;
+    }
     std::cout << "---------------------------" << std::endl;
-    std::cout << "Candidate: " << labels[result.label] << std::endl;
-    std::cout << "Score: " << result.score << std::endl;
-    std::cout << "Corner: " << result.corners.DebugString() << std::endl;
   }
 }
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
   ObjectDetection(absl::GetFlag(FLAGS_model_path),
-                absl::GetFlag(FLAGS_image_path),
-                absl::GetFlag(FLAGS_labels_path));
+                absl::GetFlag(FLAGS_images_path));
 }
